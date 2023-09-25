27b091a0bcf7ed1c628651dded1361baabbe225d
Ramses Martinez <ramses.mtz96@gmail.com>
2023-09-24 21:09:17 -0600

    First commit

---
+++ clase_02/estructuras.py
@@ -0,0 +1,33 @@
+# import this
+
+# Inputs
+# nombre = input("Ingrese su nombre: ")
+# print("Hola", nombre)
+
+# Inputs (SUMA)
+
+# Pedir el número
+# numero = int(input("Ingrese un numero a sumar: "))
+
+# suma = numero + numero
+# print(type(suma))
+# print(str(suma))
+
+
+# print(type(1))
+# print(type(-10))
+# print(type(2.0))
+# print(type(-20.0))
+# print(type("3"))
+# print(type("4"))
+# print(type('4'))
+# print(type(True))
+# print(type(False))
+# print(type([]))
+# print(type([1,2,3]))
+# print(type((1,2,3,4,5)))
+# print(type({10,20,30}))
+# print(type({"llave": "valor"}))
+# print(type({1:2}))
+
+print({10,20,30,10})
---
+++ main.py
@@ -0,0 +1,6 @@
+from clase_02 import estructuras
+
+# print("Hola Mundo!")
+# print('Hello world!')
+# print(1+2)
+# print(5-1)
